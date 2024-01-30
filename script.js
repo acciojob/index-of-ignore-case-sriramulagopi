0a25eb11045076859795be54a540a6a9df015ef7
sriramulagopi <129825285+sriramulagopi@users.noreply.github.com>
2024-01-30 12:14:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	s1=s1.toLowerCase();
+	s2=s2.toLowerCase();
+	let array = s1.split("");
+	let count = 0;
+	let index = 0;
+	for (let i=0;i<=array.length-s2.length;i++){
+		let str = s1.slice(i,i+s2.length);
+		if(str===s2){
+			index = i;
+			count++;
+		}
+	}
+	if(count>0){
+		return index;
+	}
+	return -1;
 }
 
 // Please do not change the code below
